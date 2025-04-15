41cdc2b4a1505b010c08761bff1251ec79612cdd
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2025-04-15 13:03:20 -0700

    Fix formatting and add some docs to NixOS configuration file

--- nixos/basic/configuration.nix
+++ nixos/basic/configuration.nix
@@ -6,23 +6,31 @@
     <nixos-wsl/modules>
   ];
 
+  # (NixOS-WSL configuration options)[https://nix-community.github.io/NixOS-WSL/options.html)
+  # (Advanced settings configuration in WSL)[https://learn.microsoft.com/en-us/windows/wsl/wsl-config#wslconfig]
   wsl = {
-  	enable = true;
-  	defaultUser = "mwoodpatrick";
-	interop.includePath = true;
-	};
+    enable = true;  # Whether to enable support for running NixOS as a WSL distribution.
+    defaultUser = "mwoodpatrick"; # The name of the default user
 
-  services.avahi = {
-    enable = true;
-    # Configuration options (optional, see below)
+    # This section controls interop features from within NixOS
+    interop = {
+      includePath = true; # Controls adding Windows PATH to NixOS $PATH
+      register = true; # Explicitly register the (binfmt_misc handler)[https://en.wikipedia.org/wiki/Binfmt_misc] for Windows executables
+    };
   };
-  services.automatic-timezoned.enable = true;
+
+  # services.avahi = {
+  #   enable = true;
+    # Configuration options (optional, see below)
+  # };
+
+  # services.automatic-timezoned.enable = true;
   # time.timeZone = "America/Los_Angele";
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
+  # this value at the release version of the first install of this system
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
