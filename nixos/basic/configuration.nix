27ca6f4c27dc391ec26a03177c6e7a72bad05a53
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2025-04-15 17:07:14 -0700

    Update docs

--- nixos/basic/configuration.nix
+++ nixos/basic/configuration.nix
@@ -1,3 +1,38 @@
+
+# Edit this configuration file to define what should be installed on
+# your system. 
+#
+# [NixOS & Flakes Book](https://nixos-and-flakes.thiscute.world/)
+# [NixOS Manual](https://nixos.org/manual/nixos/unstable/)
+# [NixOS on the Windows Subsystem for Linux](https://nix-community.github.io/NixOS-WSL/index.html)
+# [NixOS WSL Options](https://nix-community.github.io/NixOS-WSL/options.html)
+#
+# Search for option definitions using:
+# 
+# [Search more than 20 000 options](https://search.nixos.org/options?)
+# [WSL Configuration Options](https://nixos.org/manual/nixos/unstable/options.html)
+#
+# Configuration help:
+#
+# [Configuration Options](https://nixos.org/manual/nixos/unstable/options.html#ch-options)
+# [The Nix configuration file](https://nixos.substack.com/p/the-nix-configuration-file)
+#
+# Search fot packages using:
+#
+# [Packages](https://search.nixos.org/packages?channel=unstable)
+#
+# More detailed info is here:
+#
+# [Build Your World](https://mynixos.com/)
+# [nixos-wsl-starter](https://github.com/LGUG2Z/nixos-wsl-starter#nixos-wsl-starter)
+# [Setting Up NixOS on Windows Subsystem for Linux (WSL2)](https://www.greghilston.com/post/nixos-on-wsl/)
+# [Intro to Nixos for WSL](https://blog.jimurrito.com/nixos/2024/08/13/intro-to-nixos-for-wsl.html)
+# [NixOS on WSL](https://forrestjacobs.com/nixos-on-wsl/)
+# [Nix Flakes on WSL](https://xeiaso.net/blog/nix-flakes-4-wsl-2022-05-01/)
+# [nixos-wsl](https://github.com/mifix/nixos-wsl)
+#
+# update channels using: sudo nix-channel --update
+# rebuild using: sudo nixos-rebuild switch
 { config, lib, pkgs, ... }:
 
 {
