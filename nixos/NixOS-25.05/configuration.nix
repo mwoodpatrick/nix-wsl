21140eddd58151fd5e98a6d864904f9ceaa3bc13
Mark L. Wood-Patrick <mwoodpatrick@gmail.com>
2025-08-17 11:39:28 +0000

    Really add home manager not fully working yet

--- nixos/NixOS-25.05/configuration.nix
+++ nixos/NixOS-25.05/configuration.nix
@@ -2,6 +2,11 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
+# [NixOS Search - Packages](https://search.nixos.org/packages)
+# [NixOS Search - Options](https://search.nixos.org/options)
+
+# [NixOS tutorial - Nix Packages](https://www.youtube.com/watch?v=CqFcl4BmbN4&t=958s)
+
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
@@ -17,6 +22,165 @@
     home = "/home/mwoodpatrick";
     shell = pkgs.bash;
     extraGroups = [ "wheel" ];
+  };
+
+  # Define a specific user for Home Manager to manage
+  home-manager.users.mwoodpatrick = {
+    # It's good practice to set this to a recent version
+    home.stateVersion = "25.05"; 
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    home.packages = with pkgs; [
+      # Adds the 'hello' command to your environment. It prints a friendly
+      # "Hello, world!" when run.
+      hello
+      neovim
+      tmux
+      git
+    ];
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  /etc/profiles/per-user/mwoodpatrick/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  # Note these env vars are written to:
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    GIT_ROOT = "/mnt/wsl/projects/git";
+    KIND_EXPERIMENTAL_PROVIDER = "podman";
+    LIBGL_ALWAYS_SOFTWARE = 1; # Need for Flutter since hardware render does not work on my laptops!
+    NIX_CFG_DIR="$GIT_ROOT/nix-wsl";
+    PATH = "$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin";
+    UV_PYTHON_DOWNLOADS = "never"; # Normal python does not work in NixOS
+    # PS1=''\u@\h:\w\ myenv$ ''; # Currently trying out starship
+    DIRENV_LOG_FORMAT=""; # disable direnv output
+    # TODO: Check if running on WSL does not appear to work (check for some other env var)
+    WSL = if pkgs.hostPlatform.isWindows then "true" else "false";
+  };
+
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      # Add your custom bashrc here
+      bashrcExtra = ''
+        # added from home.nix
+        source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+        #end, added from home.nix
+      '';
+
+      # set some aliases, feel free to add more or remove some
+      shellAliases = {
+        c  = "clear";
+        h  = "history";
+        hl = "history|less";
+        ht = "history|tail -40";
+        he = "nvim $NIXOS_CONFIG_ROOT/home/home.nix";
+        hs = "home-manager switch --flake $NIXOS_CONFIG_ROOT/home;source ~/.bashrc";
+        myps = "ps -w -f -u $USER";
+        ncd = "cd $NIXOS_CONFIG_ROOT";
+        ne = "nvim $NIXOS_CONFIG_ROOT/flake.nix";
+        ns = "sudo nixos-rebuild switch --flake $NIXOS_CONFIG_ROOT#my-nix2505_wsl";
+        ngc = "nix-collect-garbage -d";
+        j = "jobs";
+        k = "kubectl";
+        urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+        urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+        wsl = "wsl.exe";
+        wterm = "/mnt/c/Program\\ Files/WezTerm/wezterm.exe &";
+      };
+    };
+
+    # Enable the Git program configuration
+    git = {
+      enable = true;
+      lfs.enable = true;
+
+      # Global user settings
+      userEmail = "mwoodpatrick@gmail.com"; # your email
+      userName = "Mark L. Wood-Patrick"; # your name
+
+      # Global Git options (equivalent to git config --global)
+      extraConfig = {
+        # Core settings
+        core = {
+          editor = "nvim"; # Your preferred Git editor (e.g., "vim", "code --wait", "nano")
+          # autoCrlf = "input"; # For consistent line endings, especially on Windows/WSL
+          # pager = "delta"; # If you use a custom pager like delta
+        };
+  
+        # Aliases
+        alias = {
+          co = "checkout";
+          br = "branch";
+          ci = "commit";
+          st = "status";
+          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        };
+  
+        # Credential helper (important for GitHub/GitLab authentication)
+        credential = {
+          helper = "store"; # Or "cache", or a more secure helper if needed
+          # store = {
+          #   helper = "cache --timeout=3600"; # Cache credentials for 1 hour
+          # };
+        };
+  
+        # Include other files (useful for organizing per-host or sensitive configs)
+        # include = {
+        #   path = "~/path/to/my/other-git-config";
+        # };
+  
+        # Protocol helpers (e.g., for GitHub CLI or specific protocols)
+        # url."git@github.com:".insteadOf = "https://github.com/";
+  
+        # Push settings
+        push = {
+          default = "current"; # Pushes the current branch to the same-named remote branch
+        };
+  
+        # Pull settings
+        pull = {
+          rebase = true; # Prefer rebase over merge for 'git pull'
+        };
+  
+        # Diff settings (e.g., using diff-so-fancy or delta)
+        # diff = {
+        #   tool = "default-difftool";
+        # };
+        # difftool."default-difftool".cmd = "nvim -d \"$LOCAL\" \"$REMOTE\"";
+  
+        # Init template directory (for custom Git hooks or templates)
+        # init = {
+        #   templatedir = "~/.git_template";
+        # };
+  
+        # Signing commits
+        # commit = {
+        #   gpgsign = true;
+        # };
+        # gpg.program = "gpg"; # Path to your GPG program
+      };
+  
+      # Or you can use specific options for common configurations:
+      # enableLfs = true; # To enable Git LFS support
+      # ignores = [ ".DS_Store" "node_modules/" ]; # Global .gitignore entries
+    };
+  };
+
   };
 
 # In the context of the `nix-community/NixOS-WSL` project, `wsl.enable` is a boolean option that specifies whether to **activate the NixOS-WSL module and all its related services and configurations**.
@@ -66,6 +230,11 @@
   # Issue: does not seem to do anything
   networking.hostName = "my-nixos-pc";
 
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   # Set your system-wide packages.
   # Allow unfree packages, which is necessary for gh.
   nixpkgs.config.allowUnfree = true;
@@ -83,6 +252,7 @@
       # For vimscript, use the `customRC` option.
       configure = {
         customRC = ''
+          set number
           set number relativenumber
           set tabstop=2
           set shiftwidth=2
