91c137e41f1c2c855f7d69f342f078c3da902f89
Mark Wood-Patrick <mwoodpatrick@gmail.com>
2025-09-18 15:25:05 -0700

    Understanding Neovim #2 - Plugins, Colorschemes

--- home-manager3/nvim/init.lua
+++ home-manager3/nvim/init.lua
@@ -15,10 +15,33 @@ opt.splitbelow = true
 
 opt.wrap = false -- Disable line wrapping
 
+-- Tabstops
+-- [indentation](https://neovim.io/doc/user/indent.html)
+-- and how many spaces are deleted when you press the <BS> (backspace) key.
+-- Pressing <Tab> will insert softtabstop spaces.
+-- Commands that indent (like >>) will insert shiftwidth spaces.
 opt.expandtab = true -- Neovim will never insert a hard tab (\t).
 opt.tabstop = 2 -- A hard tab character (\t) is rendered as 2 spaces. It is purely a display setting
+opt.shiftwidth = 2 -- Used by the auto-indent feature when you press <CR> (Enter) enters 2 spaces
+opt.softtabstop = 2 -- Defines how many spaces are inserted when you press the <Tab> key in Insert mode
 
+opt.scrolloff = 999
+
+opt.virtualedit = "block"
+
+-- Preview substitutions
+opt.inccommand = "split"
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = true -- Enable 24-bit RGB colors
+
+-- Synchronize the system clipboard
+-- with Neovim's clipboard
 if is_wsl then
+  -- Integrate with the Windows clipboard.
   -- Use win32yank when running on WSL
   vim.g.clipboard = {
     name = 'win32yank',
@@ -37,5 +60,31 @@ else
   opt.clipboard = 'unnamedplus'
 end
 
-print("init.lua loaded")
-print(vim.fn.stdpath("data"))
+-- Sets up lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- print("init.lua loaded")
+-- print(vim.fn.stdpath("data"))
+
+-- Load plugins from lua/plugins/
+require("lazy").setup("plugins", {
+-- require("lazy").setup({
+	-- Tell lazy.nvim to use a writable location for its lock file
+	-- This path should be in a user-writable directory like ~/.cache or ~/.local/share
+	lockfile = vim.fn.stdpath("cache") .. "/lazy-lock.json",
+  -- "rebelot/kanagawa.nvim"
+})
+
+vim.cmd.colorscheme("kanagawa-wave")
---
+++ home-manager3/nvim/lua/plugins/init.lua
@@ -0,0 +1,12 @@
+return {
+-- "rebelot/kanagawa.nvim"
+	require("plugins.kanagawa"),
+	-- { "nvim-tree/nvim-web-devicons" },
+	-- require("plugins.coc"),
+	-- require("plugins.conform"),
+	require("plugins.lualine"),
+	-- require("plugins.telescope"),
+	-- require("plugins.nvim-tree"),
+	-- require("plugins.lsp"),
+	-- require("plugins.null-ls"),
+}
