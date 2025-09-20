f66cf2bad0fc6c005641897020bf8cdf53c727ff
mwoodpatrick <mwoodpatrick@gmail.com>
2025-09-20 15:15:28 +0000

    Add treesitter

---
+++ home-manager3/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,42 @@
+-- [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter/tree/main)
+return {
+  'nvim-treesitter/nvim-treesitter',
+
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    ---@diagnostic disable-next-line: missing-fields
+    configs.setup({
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+          },
+        },
+      },
+      -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = { enable = true },
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      -- The auto_install setting will actually automatically install any missing parsers 
+      -- when opening a file for the first time. 
+      -- So if you don’t have rust listed in the ensure_installed, 
+      -- it will be automatically installed when you open a rust file for the first time
+      auto_install = true,
+    })
+  end
+}
