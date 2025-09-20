3979e5e26ce31222a5bb764308f3dd15b4e4a074
mwoodpatrick <mwoodpatrick@gmail.com>
2025-09-20 12:12:23 +0000

    Update Lazy to recommended configuration

--- home-manager3/nvim/lua/config/lazy.lua
+++ home-manager3/nvim/lua/config/lazy.lua
@@ -1,27 +1,44 @@
-
 -- Sets up lazy.nvim
+-- [Getting Started](https://lazy.folke.io/)
+-- [lazy.nvim](https://github.com/folke/lazy.nvim)
+-- [Nerd Fonts](https://www.nerdfonts.com/)
+-- [LuaRocks](https://luarocks.org/)
 
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- print("init.lua loaded")
--- print(vim.fn.stdpath("data"))
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
--- Load plugins from lua/plugins/
-require("lazy").setup("plugins", {
--- require("lazy").setup({
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 	-- Tell lazy.nvim to use a writable location for its lock file
 	-- This path should be in a user-writable directory like ~/.cache or ~/.local/share
 	lockfile = vim.fn.stdpath("cache") .. "/lazy-lock.json",
-  -- "rebelot/kanagawa.nvim"
 })
