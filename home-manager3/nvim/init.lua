ef3b36bd899c6330b9250b755003ecdf75f7e56a
mwoodpatrick <mwoodpatrick@gmail.com>
2025-09-20 11:41:12 +0000

    Seperate lazy setup from init.lua

--- home-manager3/nvim/init.lua
+++ home-manager3/nvim/init.lua
@@ -6,30 +6,4 @@ vim.opt.rtp:prepend(config_dir)
 
 require("config.options")
 
--- Sets up lazy.nvim
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
--- print("init.lua loaded")
--- print(vim.fn.stdpath("data"))
-
--- Load plugins from lua/plugins/
-require("lazy").setup("plugins", {
--- require("lazy").setup({
-	-- Tell lazy.nvim to use a writable location for its lock file
-	-- This path should be in a user-writable directory like ~/.cache or ~/.local/share
-	lockfile = vim.fn.stdpath("cache") .. "/lazy-lock.json",
-  -- "rebelot/kanagawa.nvim"
-})
-
+require('config.lazy')
